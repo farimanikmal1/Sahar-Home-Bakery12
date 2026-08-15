d6bc41da9e513e4744c86a45f25ec4fb6425cefb
Farima Nikmal1 <farima.nhm@gmail.com>
2026-08-15 19:00:01 +0200

    fix

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const menuButton = document.querySelector(".menu-button");
 const navbar = document.querySelector(".navbar");
 
+
+// Open / close mobile menu
 menuButton.addEventListener("click", () => {
     navbar.classList.toggle("active");
+});
+
+
+// Close menu when clicking a menu link
+const navLinks = navbar.querySelectorAll("a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navbar.classList.remove("active");
+    });
 });
